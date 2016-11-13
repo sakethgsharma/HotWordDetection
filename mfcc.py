99f31b8377057bab217575d6a56c97a195d74477
tulsianihitesh26 <tulsianihitesh26@gmail.com>
2016-11-13 22:25:34 +0530

    Changed MFFC to return 39 coefficients

--- mfcc.py
+++ mfcc.py
@@ -11,15 +11,25 @@ class MFCC:
 		self.mfcc_frame_length = round(fs*frame_dur)  #frame length for mfcc
 		self.hop_length = round(hop_dur*fs)           #hop length for mfcc 
 		self.mel_fb_sq_weights = np.genfromtxt(mel_filter_file, delimiter=',')**2  #mel filter energies
+		self.prev_frame = np.zeros((14,))
+		self.prev_prev_frame = np.zeros((14,))
 	
-	def compute_mfcc(self, input_frame):
+	def compute_mfcc(self, input_frame, include_dc=False):
 		self.windowed_frame = input_frame * np.hamming(len(input_frame)) #Hamming Windowing input frame
 		self.pre_frame = self.windowed_frame - self.alpha*np.concatenate((np.full(1, 0), self.windowed_frame[:-1])) #Preemphasis
 		self.fft_frame = np.fft.rfft(self.pre_frame,self.N) #Computing FFT
 		self.mel_frame = np.log(np.dot(self.mel_fb_sq_weights,(np.abs(self.fft_frame)**2)) + self.eps)  #Passing through mel and taking log
 		self.mel_frame_normalized = self.mel_frame / np.sum(self.mel_fb_sq_weights, axis=1)     #normalizing by filter energies
 		self.dct_frame = dct(self.mel_frame_normalized)     #Computing DCT 
-		return self.dct_frame[0:13]    # Returning first 13 DCT coefficients
+		self.delta = self.dct_frame[0:14] - self.prev_frame
+		self.double_delta = self.dct_frame[0:14] - self.prev_prev_frame
+		self.coeffs = np.append(self.dct_frame[0:14], [self.delta[1:14], self.double_delta[1:14]])
+		self.prev_prev_frame = self.prev_frame
+		self.prev_frame = self.dct_frame[0:14]
+		if (include_dc):
+			return self.coeffs  	# Returning 39 MFCC coefficients
+		else:
+			return self.coeffs[1:40]
 
 #temp = MFCC()
 #y = temp.compute_mfcc(np.random.random(200))
@@ -35,5 +45,5 @@ def test_mfcc_first_coefficient():
 	
 	for i in range(no_of_test):
 		input_frame = input_matrix[i]
-		coeffs = temp.compute_mfcc(input_frame)
+		coeffs = temp.compute_mfcc(input_frame, include_dc=True)
 		yield first_coeff_greater_than_0, coeffs[0]
