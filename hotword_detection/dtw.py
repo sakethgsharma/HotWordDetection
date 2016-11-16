1e625bc32bcdeaa2a6f6ce530fe03e8db130ac61
Prakhar1103 <prkhrswrp@gmail.com>
2016-11-16 16:44:36 +0530

    Added docstrings to dtw.py

--- hotword_detection/dtw.py
+++ hotword_detection/dtw.py
@@ -1,14 +1,45 @@
+"""This module is used for computing the DTW distance between two utterances."""
 import numpy as np
 import math
 
 def euclideanDistance(a,b):
+	"""
+
+	This function computes the simple Euclidean distance between two MFCC vectors. This is further useful in DTW distance computation.
+
+	:param a: MFCC vector 1
+	:type a: array
+	:param b: MFCC vector 2
+	:type b: array
+	:returns: Euclidean distance between MFCC vectors 1 and 2
+	:rtype: float
+
+	"""
         return np.sqrt(np.abs(np.sum(np.dot(a,a) - np.dot(b,b))))
 
 class DTW:
+	"""
+	
+	This class contains methods and parameters used for computing the DTW distance between training and test utterances. 
+
+	Documentation related to all methods in this class is described below.
+
+	"""
         def __init__(self, distFunc=euclideanDistance):
                 self.distFunc = distFunc 
 
         def compute_distance(self, reference, test):
+		"""
+		This function computes the DTW distance between two utterances by constructing a distance matrix.
+
+		:param reference: Matrix containing MFCC vectors of training utterance
+		:type reference: matrix
+		:param test: Matrix containing MFCC vectors of test utterance
+		:param test: matrix
+		:returns: DTW distance between reference and test utterance
+		:rtype: float
+ 
+		"""
                 if (type(reference) is str):
 	                reference = list(reference)
         	if (type(test) is str):
